299db6155e6925abc59470a9fb5b3bf2b51e733b
你的GitHub名稱 <GitHub>
2025-09-11 02:24:25 +0800

    骰寶

--- app/casino/sicbo/layout.tsx
+++
@@ -1,9 +0,0 @@
-// 闂佸疇娉曟刊瀵哥箔?闂備緡鍋呴悷锕€危?Server Component闂佹寧绋戦悧鎾绘偉椤栨粍鍤婃い鎾卞妿缁€鍡涙煥濞戞瀚伴柣銊ｅ劦濮婃椽鍩€椤掑倸绶ら柨鏇炲亞閳ь剙绻掗埀顑跨祷閸╂牠顢欏畝鍕闁稿﹦鍠撶粈鍡涙煕韫囧鍔ラ柛銈嗙矊椤斿繑娼忛埡鍌滄殸闂佺绻堥崝宀勬偂濞嗗精鐔兼晲閸涱垳顢?
-
-export default function SicboSectionLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
