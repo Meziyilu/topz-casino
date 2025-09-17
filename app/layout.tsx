a9d006c7195e8f51c2b6c9fd29d2a4aea169a604
你的GitHub名稱 <GitHub>
2025-09-18 07:14:30 +0800

    商店

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,15 @@ import Script from "next/script";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-Hant">
+      <head>
+        {/* 載入商店 / 頭框 / 銀行的樣式 */}
+        <link rel="stylesheet" href="/styles/shop.css" />
+        <link rel="stylesheet" href="/styles/headframes.css" />
+        <link rel="stylesheet" href="/styles/bank.css" />
+      </head>
       <body>
         {children}
+
         {/* afterInteractive：等可互動再載，避免阻塞渲染，也不會有 preload 警告 */}
         <Script
           id="tawk-embed"
